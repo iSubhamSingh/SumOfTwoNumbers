235000337179902393eeb583af0627c2c5963fa5
Subham <subhamsingh5638@gmail.com>
2024-01-08 23:17:42 +0530

    added check for not a number

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function add_number() {
-               
+     
     let first_number = parseInt(document.getElementById("firstInput").value);
     let second_number = parseInt(document.getElementById("secondInput").value);
-    let result = first_number + second_number;
 
+    if (isNaN(first_number) || isNaN(second_number)) {
+        alert("Please enter valid numbers in both fields");
+        return;
+    }
+
+    let result = first_number + second_number;
     document.getElementById("result").value = result;
 }
 
